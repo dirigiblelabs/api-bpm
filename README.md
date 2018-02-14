1cac994a3515af0b56f2aa795e8341c830933d87
delchev <delchevn@gmail.com>
2018-02-14 16:16:15 +0200

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# API V3 - Business Process Management
+
+[![Eclipse License](http://img.shields.io/badge/license-Eclipse-brightgreen.svg)](LICENSE)
+[![GitHub contributors](https://img.shields.io/github/contributors/dirigiblelabs/api-v3-bpm.svg)](https://github.com/dirigiblelabs/api-v3-bpm/graphs/contributors)
+
+## Overview
+
+* [BPM](http://www.dirigible.io/api/bpm.html)
+  - provides a set of BPM functions, for deploying, starting and overall management of the execution of business processes
+
+## License
+
+This project is copyrighted by [SAP SE](http://www.sap.com/) and is available under the [Eclipse Public License v 1.0](https://www.eclipse.org/legal/epl-v10.html). See [LICENSE](LICENSE) and [NOTICE.txt](NOTICE.txt) for further details.
---
+++ bpm/v3/engine.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+var java = require('core/v3/java');
+
+exports.getProcessEngine = function() {
+	var engineInstance = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'getProcessEngine', [], true);
+	var engine = new Engine();
+	engine.uuid = engineInstance.uuid;
+	return engine;
+};
+
+/**
+ * Engine object
+ */
+function Engine() {
+	
+}
+
+exports.startProcess = function(key, parameters) {
+	var processInstanceId = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'startProcess', [key, JSON.stringify(parameters)]);
+	return processInstanceId;
+};
+
+exports.getVariable = function(processInstanceId, variableName) {
+	var variableValue = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'getVariable', [processInstanceId, variableName]);
+	return variableValue;
+};
+
+exports.setVariable = function(processInstanceId, variableName, variableValue) {
+	java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'setVariable', [processInstanceId, variableName, variableValue]);
+};
+
+exports.removeVariable = function(processInstanceId, variableName) {
+	java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'removeVariable', [processInstanceId, variableName]);
+};
