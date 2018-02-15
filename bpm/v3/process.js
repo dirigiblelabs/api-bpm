6c33e38633cdd10c7018b4afae5c6bc0465b0351
delchev <delchevn@gmail.com>
2018-02-15 16:41:26 +0200

    execution api added

--- bpm/v3/process.js
+++ bpm/v3/process.js
@@ -12,15 +12,15 @@ var java = require('core/v3/java');
 
 exports.getProcessEngine = function() {
 	var engineInstance = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'getProcessEngine', [], true);
-	var engine = new Engine();
-	engine.uuid = engineInstance.uuid;
-	return engine;
+	var processEngine = new ProcessEngine();
+	processEngine.uuid = engineInstance.uuid;
+	return processEngine;
 };
 
 /**
- * Engine object
+ * ProcessEngine object
  */
-function Engine() {
+function ProcessEngine() {
 	
 }
 
@@ -28,6 +28,101 @@ exports.start = function(key, parameters) {
 	var processInstanceId = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'startProcess', [key, JSON.stringify(parameters)]);
 	return processInstanceId;
 };
+
+exports.getExecutionContext = function() {
+	var data = JSON.parse(context.get('execution'));
+	var execution = new ExecutionContext(data);
+	return execution;
+};
+
+/**
+ * ExecutionContext object
+ */
+function ExecutionContext(data) {
+	
+	this.getId = function() {
+		return data.id;
+	};
+	this.isActive = function() {
+		return data.isActive;
+	};
+	this.isScope = function() {
+		return data.isScope;
+	};
+	this.isConcurrent = function() {
+		return data.isConcurrent;
+	};
+	this.isEnded = function() {
+		return data.isEnded;
+	};
+	this.isEventScope = function() {
+		return data.isEventScope;
+	};
+	this.isMultiInstanceRoot = function() {
+		return data.isMultiInstanceRoot;
+	};
+	this.isCountEnabled = function() {
+		return data.isCountEnabled;
+	};
+	this.suspensionState = function() {
+		return data.suspensionState;
+	};
+	this.startTime = function() {
+		return data.startTime;
+	};
+	this.eventSubscriptionCount = function() {
+		return data.eventSubscriptionCount;
+	};
+	this.taskCount = function() {
+		return data.taskCount;
+	};
+	this.jobCount = function() {
+		return data.jobCount;
+	};
+	this.timerJobCount = function() {
+		return data.timerJobCount;
+	};
+	this.suspendedJobCount = function() {
+		return data.suspendedJobCount;
+	};
+	this.deadLetterJobCount = function() {
+		return data.deadLetterJobCount;
+	};
+	this.variableCount = function() {
+		return data.variableCount;
+	};
+	this.identityLinkCount = function() {
+		return data.identityLinkCount;
+	};
+	this.processDefinitionId = function() {
+		return data.processDefinitionId;
+	};
+	this.processDefinitionKey = function() {
+		return data.processDefinitionKey;
+	};
+	this.activityId = function() {
+		return data.activityId;
+	};
+	this.processInstanceId = function() {
+		return data.processInstanceId;
+	};
+	this.parentId = function() {
+		return data.parentId;
+	};
+	this.rootProcessInstanceId = function() {
+		return data.rootProcessInstanceId;
+	};
+	this.forcedUpdate = function() {
+		return data.forcedUpdate;
+	};
+	this.revision = function() {
+		return data.revision;
+	};
+	this.tenantId = function() {
+		return data.tenantId;
+	};
+	
+}
 
 exports.getVariable = function(processInstanceId, variableName) {
 	var variableValue = java.call('org.eclipse.dirigible.api.v3.bpm.BpmFacade', 'getVariable', [processInstanceId, variableName]);
