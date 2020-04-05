9a9304995c362c6307cd844e4b33ed6df48af4e1
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:43:14 +0300

    Adds "api-modules" Extension

---
+++ bpm/extensions/modules.js
@@ -0,0 +1,30 @@
+exports.getContent = function() {
+	return [{
+		name: "bpm/v4/deployer",
+		description: "BPM Deployer API"
+	}, {
+		name: "bpm/v4/process",
+		description: "BPM Process API"
+	}, {
+		name: "bpm/v4/tasks",
+		description: "BPM Tasks API"
+	}, {
+		name: "utils/v4/escape",
+		description: "Escape API"
+	}, {
+		name: "utils/v4/hex",
+		description: "Hex API"
+	}, {
+		name: "utils/v4/jsonpath",
+		description: "JsonPath API"
+	}, {
+		name: "utils/v4/url",
+		description: "URL API"
+	}, {
+		name: "utils/v4/uuid",
+		description: "UUID API"
+	}, {
+		name: "utils/v4/xml",
+		description: "XML API"
+	}];
+};
