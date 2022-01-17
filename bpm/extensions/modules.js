1d43f660a557a6b0ad5c5e7cd634b6cd55b92727
Ivan <iwolkkov@gmail.com>
2022-01-17 17:49:58 +0200

    d.ts added

--- bpm/extensions/modules.js
+++ bpm/extensions/modules.js
@@ -1,22 +1,17 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
-exports.getContent = function() {
-	return [{
-		name: "bpm/v4/deployer",
-		description: "BPM Deployer API"
-	}, {
-		name: "bpm/v4/process",
-		description: "BPM Process API"
-	}, {
-		name: "bpm/v4/tasks",
-		description: "BPM Tasks API"
-	}];
+let platform = require("platform/v4/registry")
+exports.getContent = function () {
+    let file = platform.getText("bpm/extensions/modules.json");
+    return JSON.parse(file);
 };
+
